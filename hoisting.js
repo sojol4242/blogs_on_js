f21a0a359ccbb2317996a0c671629bffb025e012
Md Moniruzzaman <bsmrstueee90@gmail.com>
2022-01-31 00:56:46 +0600

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,24 @@
+// getName();
+// console.log(x);
+//  console.log(getName);
+
+var x = 20;
+//  function getName(){
+//      console.log('Sojol');
+//  }
+
+var getName = () => {
+  console.log("Sojol");
+};
+
+getName();
+console.log(x);
+// const addOne = (value) => value + 1;
+// const addTwo = (value) => addOne(value + 1);
+// const addThree = (value) => addTwo(value + 1);
+// const calculation = () => {
+   
+//   const sum = addThree(1) + addTwo(2);
+//   return sum;
+// };
+// console.log(calculation());
